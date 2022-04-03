d2c40a280fb83513b540709a198af66ad68be206
payload <100224467+payloadinserted@users.noreply.github.com>
2022-04-04 01:05:02 +0200

    Add files via upload

---
+++ DLLInjector.java
@@ -0,0 +1,46 @@
+public class DLLInjector {
+    public static void main(String[] args) throws Exception {
+        String dllPath = "C:\\Users\\Administrator\\Desktop\\dll\\test.dll";
+        String targetProcessName = "notepad";
+        String targetWindowName = "";
+        String targetWindowClass = "";
+        String targetWindowTitle = "";
+        String targetWindowText = "";
+        String targetWindowClassName = "";
+        String targetWindowClassName2 = "";
+        String targetWindowClassName3 = "";
+        String targetWindowClassName4 = "";
+        String targetWindowClassName5 = "";
+        String targetWindowClassName6 = "";
+        String targetWindowClassName7 = "";
+        String targetWindowClassName8 = "";
+        String targetWindowClassName9 = "";
+        String targetWindowClassName10 = "";
+        String targetWindowClassName11 = "";
+        String targetWindowClassName12 = "";
+        String targetWindowClassName13 = "";
+        String targetWindowClassName14 = "";
+        String targetWindowClassName15 = "";
+        String targetWindowClassName16 = "";
+        String targetWindowClassName17 = "";
+        String targetWindowClassName18 = "";
+        String targetWindowClassName19 = "";
+        String targetWindowClassName20 = "";
+        String targetWindowClassName21 = "";
+        String targetWindowClassName22 = "";
+        String targetWindowClassName23 = "";
+        String targetWindowClassName24 = "";
+        String targetWindowClassName25 = "";
+        String targetWindowClassName26 = "";
+        String targetWindowClassName27 = "";
+        String targetWindowClassName28 = "";
+        String targetWindowClassName29 = "";
+        String targetWindowClassName30 = "";
+        String targetWindowClassName31 = "";
+        String targetWindowClassName32 = "";
+        String targetWindowClassName33 = "";
+        String targetWindowClassName34 = "";
+        String targetWindowClassName35 = "";
+        String targetWindowClassName36 = "";
+        String targetWindowClassName37 = "";
+        String targetWindowClassName
